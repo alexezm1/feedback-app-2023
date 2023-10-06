bd8e34700709b8b7270c0a2438232f4f5081952d
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-06 16:25:45 -0700

    Form text validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,20 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState();
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (e.target.value === "") {
+      setBtnDisabled(true);
+      setMessage("");
+    } else if (e.target.value !== "" && e.target.value.trim().length <= 10) {
+      setMessage("Review must be at least 10 characters long");
+      setBtnDisabled(true);
+    } else {
+      setMessage("");
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
@@ -20,8 +32,12 @@ function FeedbackForm() {
             type="text"
             placeholder="Write a review"
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
