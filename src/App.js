498ae52e870ebd00e65dede93011b4e80d944564
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-05 18:15:51 -0700

    delete Feedback event with prop drilling

--- src/App.js
+++ src/App.js
@@ -7,10 +7,20 @@ import FeedbackList from "./components/FeedbackList";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(
+        feedback.filter((item) => {
+          return item.id !== id;
+        })
+      );
+    }
+  };
+
   return (
     <>
       <Header />
-      <FeedbackList feedback={feedback} />
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
     </>
   );
 }
