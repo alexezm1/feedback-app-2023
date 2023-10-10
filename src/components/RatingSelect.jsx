489f6d140c956e9169010087a2c0964a61484e23
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-09 18:16:32 -0700

    fixed select bug

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function RatingSelect({ select, selected }) {
   const handleChange = (e) => {
-    select(e.currentTarget.value);
+    select(+e.currentTarget.value);
   };
   return (
     <ul className="rating">
