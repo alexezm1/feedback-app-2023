643db1a123ec0dffcf3a7819811e912021d4e79a
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-11 12:23:32 -0700

    fetch data from json mock backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,13 +1,27 @@
-import { createContext, useState } from "react";
-import FeedbackData from "../data/FeedbackData";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, [feedback]);
+
+  // Fetch Feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    );
+
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   // Add Feedback item
   const addFeedback = (newFeedback) => {
@@ -49,6 +63,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         addFeedback,
         deleteFeedback,
         editFeedback,
