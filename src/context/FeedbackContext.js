bef2b6e456996df530393f56d7440e1c443345cd
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-12 12:05:35 -0700

    Add feedback with json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -14,9 +13,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch Feedback
   const fetchFeedback = async () => {
-    const response = await fetch(
-      `http://localhost:5000/feedback?_sort=id&_order=desc`
-    );
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
 
     const data = await response.json();
     setFeedback(data);
@@ -24,9 +21,18 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Add Feedback item
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch(`/feedback`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+    console.log(data);
+    setFeedback([data, ...feedback]);
   };
 
   // Delete Feedback item
