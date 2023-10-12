21563f3ab9b775a6052da521df526a26e92ab6e5
Alejandro Zamora <alex_speed_95@hotmail.com>
2023-10-12 12:13:35 -0700

    update and delete with json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -36,8 +36,11 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Delete Feedback item
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, {
+        method: "DELETE",
+      });
       setFeedback(
         feedback.filter((item) => {
           return item.id !== id;
@@ -47,11 +50,18 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update feedback item
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedItem),
+    });
+
+    const data = await response.json();
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
-      )
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
     setFeedbackEdit({ item: {}, edit: false });
   };
